9ad171333637df908ce19b36911c17ca44d94983
cliffordwilliam <131695663+cliffordwilliam@users.noreply.github.com>
2024-10-02 09:53:25 +0700

    Update body-scroll-animation.js
    simpler logic for h1 animation

--- assets/js/body-scroll-animation.js
+++ assets/js/body-scroll-animation.js
@@ -1,13 +1,6 @@
-const bodyObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-    })
+window.addEventListener('load', () => {
+    const pageH1 = document.getElementById("h1");
+    if (pageH1) {
+        pageH1.classList.add("show");
+    }
 });
-
-const pageH1 = document.querySelector("#h1");
-if (pageH1) {
-    pageH1.classList.add("hidden");
-    bodyObserver.observe(pageH1);
-}
