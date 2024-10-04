632eccded6ee7e6a9973ddb8e7cb29a363e3630a
Clifford William <ccliffordwilliam@gmail.com>
2024-10-05 03:14:22 +0700

    use my posts not hardcode

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,13 @@
-let documents = [{
-    "name": "Lunr",
-    "text": "Like Solr, but much smaller, and not as bright."
-}, {
-    "name": "React",
-    "text": "A JavaScript library for building user interfaces."
-}, {
-    "name": "Lodash",
-    "text": "A modern JavaScript utility library delivering modularity, performance & extras."
-}]
+let documents = [
+    {% for post in site.posts %}
+    {
+        "name": "{{ post.title }}",
+        "text": "{{ post.description }}",
+        "categories": "{{ post.categories | join: ', ' }}",
+        "image": "{{ post.image }}"
+    }{% if forloop.last == false %},{% endif %}
+    {% endfor %}
+];
 let idx = lunr(function () {
     this.ref('name')
     this.field('text')
