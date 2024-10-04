e5758248efab084ad97748ed21c3e31f63702b95
Clifford William <ccliffordwilliam@gmail.com>
2024-10-05 02:48:26 +0700

    hard code lunr testing move to fetch

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,61 +1,19 @@
-(function () {
-    function showResults(results, store) {
-        let searchResults = document.getElementById('search-results');
-
-        if (results.length) { // If there are results...
-            let appendString = '';
-
-            for (let i = 0; i < results.length; i++) {  // Iterate over them and generate html
-                let item = store[results[i].ref];
-                appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-                appendString += '<p>' + item.content.substring(0, 250) + '...</p></li>';
-            }
-
-            searchResults.innerHTML = appendString;
-        } else {
-            searchResults.innerHTML = '<li>No results found</li>';
-        }
-    }
-
-    function getQuery(variable) {
-        let query = window.location.search.substring(1);
-        let vars = query.split('&');
-
-        for (let i = 0; i < vars.length; i++) {
-            let pair = vars[i].split('=');
-
-            if (pair[0] === variable) {
-                return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-            }
-        }
-    }
-
-    let searchTerm = getQuery('query');
-
-    if (searchTerm) {
-        document.getElementById('search-box').setAttribute("value", searchTerm);
-
-        // Initalize lunr.js with the fields to search.
-        // The title field is given more weight with the "boost" parameter
-        let idx = lunr(function () {
-            this.field('id');
-            this.field('title', { boost: 10 });
-            this.field('author');
-            this.field('category');
-            this.field('content');
-
-            for (let key in window.store) { // Add the JSON we generated from the site content to Lunr.js.
-                this.add({
-                    'id': key,
-                    'title': window.store[key].title,
-                    'author': window.store[key].author,
-                    'category': window.store[key].category,
-                    'content': window.store[key].content
-                });
-            }
-        });
-
-        let results = idx.search(searchTerm); // Perform search with Lunr.js
-        showResults(results, window.store);
-    }
-})();
+let documents = [{
+    "name": "Lunr",
+    "text": "Like Solr, but much smaller, and not as bright."
+  }, {
+    "name": "React",
+    "text": "A JavaScript library for building user interfaces."
+  }, {
+    "name": "Lodash",
+    "text": "A modern JavaScript utility library delivering modularity, performance & extras."
+  }]
+  let idx = lunr(function () {
+    this.ref('name')
+    this.field('text')
+  
+    documents.forEach(function (doc) {
+      this.add(doc)
+    }, this)
+  })
+  alert(idx.search("bright"))
